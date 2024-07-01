901149050c9043cedaf1edae32aa147c9beb9272
Flames <isaacomenuche@gmail.com>
2024-07-01 21:14:22 +0100

    V1

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const image = document.querySelector("img");
+function displayCurrentDayAndUTCTime() {
+  var now = new Date();
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var currentDay = days[now.getUTCDay()];
+  var currentTime = now.toUTCString().split(" ")[4];
 
-image.addEventListener("load", (event) => {
-  const { naturalWidth, naturalHeight, width, height } = image;
-});
+  document.getElementById("current-day").innerText = currentDay;
+  document.getElementById("current-time").innerText = currentTime;
+}
+
+displayCurrentDayAndUTCTime();
+setInterval(displayCurrentDayAndUTCTime, 1000);
